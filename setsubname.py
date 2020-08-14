c8f827165ea6de9abccf64f114dd7d0c8d255645
Heonyoung <yopeule@naver.com>
2020-08-15 08:47:29 +0900

    dict 실수 확인

--- setsubname.py
+++ setsubname.py
@@ -1,37 +1,39 @@
 import os, sys
-l = [i for i in os.listdir('.') if sys.argv[0].split('\\')[-1] != i]
-extend_name = set() 
-for i in l:
-    extend_name.add(i)
-# ll = [[], []]
-ll = {}
-for ename in extend_name:
-    ll[ename] = []
+
+dir_list = [i for i in os.listdir('.') if sys.argv[0].split('\\')[-1] != i]
+
+extensions = set() 
+for i in dir_list:
+    extensions.add(i.split('.')[-1])
+
+dir_list_dict_by_extension = {}
+
+for extension in extensions:
+    dir_list_dict_by_extension[extension] = []
 # for ename in extend_name:
 #     ll.append([i for i in l if i.split('.')[-1] == ename])
+for i in dir_list:
+    for extension in extensions:
+        if i.split('.')[-1] == extension:
+            dir_list_dict_by_extension[extension].append(i)
 
-for i in l:
-    for ename in extend_name:
-        if i.split('.')[-1] == ename:
-            ll[ename].append(i)
-
-for i in ll:
+for i in dir_list_dict_by_extension:
     i.sort()
-    # i 가 왜 문자열이라고 하는거지? ??? 
+    # i 가 왜 문자열이라고 하는거지? ??? 아... key 를 순회하는구나...
 # k = ll['a']
 
 def get_rename_only_name(origin_name, rename_name):
     return rename_name.split('.')[0:-2] + origin_name.split('.')[-1]
 
-ename = [i for i in extend_name]
+extension = [i for i in extensions]
 
 # for i in range(len(ll[0])):
 #     os.rename(ll[ename[0]][i], get_rename_only_name(ll[ename[0]][i], ll[ename[1]][i]))
 
-for i in range(len(ll[0])):
-    a = extend_name.pop()
-    for j in extend_name:
-        os.rename(ll[j][i], get_rename_only_name(ll[j][i], ll[a][i]))
+for i in range(len(dir_list_dict_by_extension[0])):
+    a = extensions.pop()
+    for j in extensions:
+        os.rename(dir_list_dict_by_extension[j][i], get_rename_only_name(dir_list_dict_by_extension[j][i], dir_list_dict_by_extension[a][i]))
 
 # 확장자를 기준으로 분리 ?
 # 어차피 파일명은 재생 순서를 위해 사전순으로 만들어져있다. 
